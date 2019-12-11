e64cfe4d442695c77788f79861bbe8bd30163bfb
I.Tai <45036570+IEONGIONTAI@users.noreply.github.com>
2019-12-12 02:55:56 +0800

    Update 12521 - Break my heart.cpp

--- 12521 - Break my heart.cpp
+++ 12521 - Break my heart.cpp
@@ -1,40 +1,61 @@
 #include <iostream>
 #include <set>
+#include <string>
 
 using namespace std;
 
+class Solution{
+private:
+    set<int> heart;
+public:
+    void insert(int x) {
+        heart.insert(x);
+    }
+    void print() {
+        if(!heart.empty()) {
+            cout << *heart.begin();
+            for(auto i:heart) {
+                if(i != *heart.begin()) {
+                    cout << " ";
+                    cout << i;
+                }
+            }
+            cout << endl;
+        }
+    }
+    void min() {
+        if(!heart.empty()) {
+            auto temp = heart.begin();
+            cout << *temp << endl;
+        }
+    }
+    void range_erase(int l, int r) {
+        heart.erase(heart.lower_bound(l), heart.upper_bound(r));
+    }
+};
+
 int main() {
-    set<int> a  ;
+    Solution ans;
     int time;
     cin >> time;
     while(time--) {
         string cmd;
         cin >> cmd;
-        if(cmd == "insert") {
-            int b;
-            cin >> b;
-            a.insert(b);
-        }
-        else if(cmd == "print") {
-            if(!a.empty()) {
-                for(auto i:a){
-                    if(i != *a.begin())
-                        cout << " ";
-                    cout << i;
-                }
-                cout<<endl;
-            }
-        }
-        else if(cmd == "min") {
-            if(!a.empty()) {
-                auto i = a.begin();
-                cout << *i << endl;
-            }
+        if(cmd == "print") {
+            ans.print();
         }
         else if(cmd == "range_erase") {
-            int lower, upper;
-            cin >> lower >> upper;
-            a.erase(a.lower_bound(lower), a.upper_bound(upper));
+            int l, r;
+            cin >> l >> r;
+            ans.range_erase(l, r);
+        }
+        else if(cmd == "insert") {
+            int x;
+            cin >> x;
+            ans.insert(x);
+        }
+        else if(cmd == "min") {
+            ans.min();
         }
     }
     return 0;
